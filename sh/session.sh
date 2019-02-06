b707b14a90bdfba48190f7c50d42777fc4a18a4c
Rafael Timbó <timbo.rafa@gmail.com>
2019-02-06 12:28:06 -0500

    add sample docker session

---
+++ sh/session.sh
@@ -0,0 +1,31 @@
+#!/bin/bash - 
+#===============================================================================
+#
+#          FILE: session.sh
+# 
+#         USAGE: ./session.sh 
+# 
+#   DESCRIPTION: 
+# 
+#       OPTIONS: ---
+#  REQUIREMENTS: ---
+#          BUGS: ---
+#         NOTES: ---
+#        AUTHOR: YOUR NAME (), 
+#  ORGANIZATION: 
+#       CREATED: 02/ 6/2019 12:06
+#      REVISION:  ---
+#===============================================================================
+
+set -o nounset                              # Treat unset variables as an error
+set -x
+set -e
+
+CONTAINER_NAME="watcher-$$"
+docker build -t timbo/alive .
+mintty -p left docker run --name $CONTAINER_NAME timbo/alive &
+sleep 1s
+mintty -p center docker exec -i $CONTAINER_NAME /bin/bash -c "while true; do ./sh/alive.sh; done" &
+sleep 18s
+mintty -p right docker exec -i $CONTAINER_NAME /sh/stop.sh
+wait
